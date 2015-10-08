749a820fa294a6d53edf04d4551c20e9433ee48f
Preston Edmands <pedmands@gmail.com>
2015-10-08 04:52:27 -0400

    Autoprefixer installed

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt){
 					sourcemap: 'none',
 				}, // options
 				files: {
-					'compiled/style.css': 'sass/style.scss'
+					'compiled/style-human.css': 'sass/style.scss'
 				} // files
 			}, // dev
 			dist: {
@@ -19,20 +19,33 @@ module.exports = function(grunt){
 					sourcemap: 'none',
 				}, // options
 				files: {
-					'compiled/style-min.css': 'sass/style.scss'
+					'compiled/style.css': 'sass/style.scss'
 				} // files
 			} // dev
 		}, // sass
 
+		autoprefixer: {
+			options: {
+				browsers: ['last 2 versions']
+			}, // options
+			multiple_files: {
+				expand: true,
+				flatten: true,
+				src: 'compiled/*.css',
+				dest: ''
+			} // multiple_files
+		}, // autoprefixer
+
 		watch: {
 			css: {
 				files: '**/*.scss',
-				tasks: ['sass']
+				tasks: ['sass', 'autoprefixer']
 			}// css
 		}// watch
 
 	}); //initConfig
 
+	grunt.loadNpmTasks('grunt-autoprefixer');
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.registerTask('default', ['watch']);
