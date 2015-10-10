a1010e7feb03b93ff7d3acfd8ecf02302b85122d
Preston Edmands <pedmands@gmail.com>
2015-10-10 13:00:17 -0400

    Fixed nav

--- header.php
+++ header.php
@@ -24,27 +24,28 @@
 <div id="page" class="hfeed site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'undersoressass' ); ?></a>
 
-	<header id="masthead" class="site-header fullheight" role="banner">
-<div class="nav-logo"><h1>Preston Edmands</h1></div>
+
+	<?php if ( is_front_page() && is_home() ) : ?>
+		<header id="masthead" class="site-header fullheight" role="banner">
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><i class="fa fa-bars"></i></button>
-			
+			<div class="nav-logo"><h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1></div>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-
 		</nav><!-- #site-navigation -->
-
 		<div class="site-branding">
 			<div class="title-box">
-				<?php if ( is_front_page() && is_home() ) : ?>
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php else : ?>
-					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php endif; ?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 				<p class="site-description"><?php bloginfo( 'description' ); ?></p>
-			</div>
+			</div><!-- title-box -->
 		</div><!-- .site-branding -->
-
-		
+	<?php else : ?>
+		<header id="masthead" class="site-header" role="banner">
+		<nav id="site-navigation" class="main-navigation" role="navigation">
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><i class="fa fa-bars"></i></button>
+			<div class="nav-logo"><h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1></div>
+			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+		</nav><!-- #site-navigation -->
+	<?php endif; ?>
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
