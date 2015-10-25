36545f5dd432f3b1762dc64f86a2bf7b82eec29c
Preston Edmands <pedmands@gmail.com>
2015-10-24 21:34:15 -0400

    Fixed masonry

--- js/masonry-settings.js
+++ js/masonry-settings.js
@@ -34,80 +34,80 @@ jQuery(document).ready(function($){
     
 });
 
-// Masonry settings to organize portfolio
+// // Masonry settings to organize portfolio
+// jQuery(document).ready(function($){
+//     var $container = $('#websites');
+//     var $masonryOn;
+//     var $columnWidth = '.portfolio';
+    
+//     if ($(document).width() > 879) {
+//         $masonryOn = true;
+//         runPortfolioMasonry();
+//     };
+
+//     $(window).resize( function() {
+//         if ($(document).width() < 379) {
+//             if ($masonryOn){
+//                 $container.masonry('destroy');
+//                 $masonryOn = false;
+//             }
+
+//         } else {
+//             $masonryOn = true;
+//             runPortfolioMasonry();
+//         }
+//     });
+    
+//     function runPortfolioMasonry() {
+//         // initialize
+//         $container.masonry({
+//             columnWidth: $columnWidth,
+//             gutter: 20,
+//             itemSelector: '.portfolio',
+//             isFitWidth: true,
+//             isAnimated: true
+//         });
+//     };
+    
+// });
+// jQuery(document).ready(function($){
+//     var $container = $('#print');
+//     var $masonryOn;
+//     var $columnWidth = '.portfolio';
+    
+//     if ($(document).width() > 879) {
+//         $masonryOn = true;
+//         runPortfolioMasonry();
+//     };
+
+//     $(window).resize( function() {
+//         if ($(document).width() < 379) {
+//             if ($masonryOn){
+//                 $container.masonry('destroy');
+//                 $masonryOn = false;
+//             }
+
+//         } else {
+//             $masonryOn = true;
+//             runPortfolioMasonry();
+//         }
+//     });
+    
+//     function runPortfolioMasonry() {
+//         // initialize
+//         $container.masonry({
+//             columnWidth: $columnWidth,
+//             gutter: 20,
+//             itemSelector: '.portfolio',
+//             isFitWidth: true,
+//             isAnimated: true
+//         });
+//     };
+    
+// });
+
 jQuery(document).ready(function($){
-    var $container = $('#websites');
-    var $masonryOn;
-    var $columnWidth = '.portfolio';
-    
-    if ($(document).width() > 879) {
-        $masonryOn = true;
-        runPortfolioMasonry();
-    };
-
-    $(window).resize( function() {
-        if ($(document).width() < 379) {
-            if ($masonryOn){
-                $container.masonry('destroy');
-                $masonryOn = false;
-            }
-
-        } else {
-            $masonryOn = true;
-            runPortfolioMasonry();
-        }
-    });
-    
-    function runPortfolioMasonry() {
-        // initialize
-        $container.masonry({
-            columnWidth: $columnWidth,
-            gutter: 20,
-            itemSelector: '.portfolio',
-            isFitWidth: true,
-            isAnimated: true
-        });
-    };
-    
-});
-jQuery(document).ready(function($){
-    var $container = $('#print');
-    var $masonryOn;
-    var $columnWidth = '.portfolio';
-    
-    if ($(document).width() > 879) {
-        $masonryOn = true;
-        runPortfolioMasonry();
-    };
-
-    $(window).resize( function() {
-        if ($(document).width() < 379) {
-            if ($masonryOn){
-                $container.masonry('destroy');
-                $masonryOn = false;
-            }
-
-        } else {
-            $masonryOn = true;
-            runPortfolioMasonry();
-        }
-    });
-    
-    function runPortfolioMasonry() {
-        // initialize
-        $container.masonry({
-            columnWidth: $columnWidth,
-            gutter: 20,
-            itemSelector: '.portfolio',
-            isFitWidth: true,
-            isAnimated: true
-        });
-    };
-    
-});
-
-jQuery(document).ready(function($){
-    var $container = $('#identity');
+    var $container = $('.tab');
     var $masonryOn;
     var $columnWidth = '.portfolio';
     
